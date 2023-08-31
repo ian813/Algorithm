60fe2c075d334ec90e1844154e68bbeadc688a7a
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-08-31 09:24:56 +0900

    [Gold V] Title: 용액, Time: 844 ms, Memory: 98004 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+/**
+ * 오름차순으로 주어져서 더 쉽게 구할 수 있다.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int[] solution = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			solution[i] = sc.nextInt();
+		}
+
+		// 시작 인덱스, 끝 인덱스
+		int start = 0;
+		int end = N - 1;
+		// 합
+		int sum = 0;
+		// 합이 0과 가까울 때 최소값, 최대값
+		int min = 0;
+		int max = 0;
+		// 0과 가까운 sum으로 계속 업뎃해줄 값
+		int dist = Integer.MAX_VALUE;
+
+		// 시작 인덱스가 끝 인덱스보다 작을 때까지만 탐색
+		while (start < end) {
+			// sum은 지금 가리키는 두 포인터의 합으로 초기화
+			sum = solution[start] + solution[end];
+
+			// sum의 절댓값이 dist의 절댓값보다 작으면
+			if (Math.abs(sum) < Math.abs(dist)) {
+				// dist 업뎃하고
+				dist = sum;
+				// 최소 최대값을 각각 업뎃
+				min = solution[start];
+				max = solution[end];
+			}
+
+			if (sum < 0) {
+				// sum이 0보다 작으면
+				// 작은 값을 늘려줘야 하므로 start++
+				start++;
+			} else if (sum > 0) {
+				// sum이 0보다 크면
+				// 큰 값을 줄여줘야 하므로 end--
+				end--;
+			} else {
+				// sum이 0이면 멈추기
+				break;
+			}
+		}
+
+		// 출력
+		System.out.println(min + " " + max);
+	}
+}
