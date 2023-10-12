893789989803d63362d3f2cd7f93a8b20c2bb526
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-10-12 13:47:48 +0900

    [Silver V] Title: 행렬 곱셈, Time: 148 ms, Memory: 15812 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -1,29 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		// 행렬 정보와 행렬 생성해서 입력받기
-		int row1 = sc.nextInt();
-		int col1 = sc.nextInt();
+		int row1 = Integer.parseInt(st.nextToken());
+		int col1 = Integer.parseInt(st.nextToken());
 		int[][] matrix1 = new int[row1][col1];
 
 		for (int i = 0; i < row1; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < col1; j++) {
-				matrix1[i][j] = sc.nextInt();
+				matrix1[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		int row2 = sc.nextInt();
-		int col2 = sc.nextInt();
+		st = new StringTokenizer(br.readLine());
+
+		int row2 = Integer.parseInt(st.nextToken());
+		int col2 = Integer.parseInt(st.nextToken());
 
 		int[][] matrix2 = new int[row2][col2];
 
 		for (int i = 0; i < row2; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < col2; j++) {
-				matrix2[i][j] = sc.nextInt();
+				matrix2[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
@@ -33,7 +41,6 @@ public class Main {
 		// 곱셈 구현
 		for (int i = 0; i < row1; i++) {
 			for (int j = 0; j < col2; j++) {
-				result[i][j] = 0;
 				for (int k = 0; k < col1; k++) {
 					// 잘 생각해 보면 나온다...
 					result[i][j] += (matrix1[i][k] * matrix2[k][j]);
