fd63ac4a77ead0954984ac4ba40abf2a85ddb47d
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-11-25 14:21:09 +0900

    [Silver III] Title: 주유소, Time: 2016 ms, Memory: 40248 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,51 +1,74 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		// 도시 개수
-		int city = sc.nextInt();
+		int city = Integer.parseInt(st.nextToken());
 
 		// 도로 길이와 가격 정보 받을 배열
 		int[] road = new int[city];
 
 		int[] price = new int[city];
 
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < road.length - 1; i++) {
-			road[i] = sc.nextInt();
+			road[i] = Integer.parseInt(st.nextToken());
 		}
 
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < price.length; i++) {
-			price[i] = sc.nextInt();
+			price[i] = Integer.parseInt(st.nextToken());
 		}
 
-		// 최소 비용, 최소 가격, 도로 길이 저장할 변수
-		long minCost = 0;
-		long minPrice = 1000000001;
-		long roadLength = 0;
+		// 최소값을 갖는 인덱스, 끝 인덱스
+		int minIdx = city;
+		int endIdx = city - 1;
 
-		for (int i = price.length - 1; i >= 0; i--) {
-			if (price[i] <= minPrice) {
-				// 더 작은 가격 발견하면
-				// 최소 가격 업데이트 및 도로 길이 누적
-				minPrice = price[i];
+		// 최소 비용
+		Long minCost = 0L;
+
+		// 최소값 인덱스가 0보다 클때까지만 반복
+		while (minIdx > 0) {
+			// 최소값 인덱스 찾아주기
+			minIdx = findMin(0, endIdx, price);
+
+			// 누적시킬 도로 길이
+			Long roadLength = 0L;
+
+			for (int i = minIdx; i <= endIdx; i++) {
+				// 최소값인덱스부터 끝 인덱스까지 도로 누적
 				roadLength += road[i];
-			} else {
-				// 더 큰 가격 발견하면
-				// 지금까지 누적한 도로 길이와 최소 가격 이용해서 비용 구해주고
-				minCost += (minPrice * roadLength);
-				// 가격하고 도로 길이 업데이트
-				minPrice = price[i];
-				roadLength = road[i];
+			}
+
+			// 최소 비용 구하고 끝 인덱스 초기화
+			minCost += roadLength * price[minIdx];
+			endIdx = minIdx - 1;
+		}
+
+		// 최소 비용 출력
+		System.out.println(minCost);
+	}
+
+	// 최솟값과 최솟값 위치 구하는 메서드
+	private static int findMin(int startIdx, int endIdx, int[] arr) {
+		// 최소 가격과 그 위치 저장
+		int min = arr[startIdx];
+		int minIdx = startIdx;
+
+		// 최소값 찾기
+		for (int i = startIdx + 1; i <= endIdx; i++) {
+			if (min > arr[i]) {
+				min = arr[i];
+				minIdx = i;
 			}
 		}
 
-		// 마지막 남은 비용 구해주기
-		minCost += minPrice * roadLength;
-
-		// 출력
-		System.out.println(minCost);
+		return minIdx;
 	}
 }
