ec7079de53625f8e13d413d6084e522dcf78c1a9
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-11-28 09:28:42 +0900

    [Silver V] Title: 배열 합치기, Time: 1676 ms, Memory: 317056 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,25 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		// 배열 크기 두개 입력받기
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
 		// 두 배열은 정렬되어 있으므로 두 포인터를 써보자.
 		int[] A = new int[N];
 		int[] B = new int[M];
 
+        st = new StringTokenizer(br.readLine());
 		// 두 배열 입력받기
 		for (int i = 0; i < N; i++) {
-			A[i] = sc.nextInt();
+			A[i] = Integer.parseInt(st.nextToken());
 		}
 
+        st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
-			B[i] = sc.nextInt();
+			B[i] = Integer.parseInt(st.nextToken());
 		}
 
 		// 포인터 두개 설정
