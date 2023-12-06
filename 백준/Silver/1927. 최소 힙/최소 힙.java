08767053480aaca0eba4818f31be54e442266b4e
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-12-07 08:39:33 +0900

    [Silver II] Title: 최소 힙, Time: 332 ms, Memory: 25988 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,21 +1,23 @@
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		PriorityQueue<Integer> queue = new PriorityQueue<>();
 
 		// 반복할 횟수
-		int re = sc.nextInt();
+		int re = Integer.parseInt(br.readLine());
 
 		StringBuilder sb = new StringBuilder();
 
 		while (re-- > 0) {
 			// 현재 값 입력받기
-			int cur = sc.nextInt();
+			int cur = Integer.parseInt(br.readLine());
 
 			if (cur == 0) {
 				// 입력받은 값이 0이면
