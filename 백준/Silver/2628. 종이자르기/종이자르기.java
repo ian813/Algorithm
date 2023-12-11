14ce33e558ba99cdd44e4eecf6bb8ec59c1e3fb7
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-12-11 11:10:53 +0900

    [Silver V] Title: 종이자르기, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Silver/2628. 종이자르기/종이자르기.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 가로 세로 길이
+		int width = sc.nextInt();
+		int height = sc.nextInt();
+
+		// 자르는 횟수
+		int cut = sc.nextInt();
+
+		// 세로 가로 잘리는 지점 저장할 리스트
+		ArrayList<Integer> horizon = new ArrayList<>();
+		ArrayList<Integer> vertical = new ArrayList<>();
+
+		while (cut-- > 0) {
+			// 자르는 방향
+			int direction = sc.nextInt();
+
+			// 잘리는 위치 저장
+			if (direction == 0) {
+				vertical.add(sc.nextInt());
+			} else {
+				horizon.add(sc.nextInt());
+			}
+		}
+
+		// 처음 좌표 넣어주기
+		vertical.add(0);
+		horizon.add(0);
+
+		// 끝 좌표 넣어주기
+		vertical.add(height);
+		horizon.add(width);
+
+		// 오름차순 정렬
+		Collections.sort(vertical);
+		Collections.sort(horizon);
+
+		// 최대 넓이
+		// 최대 가로 세로 길이 초기화
+		int maxWidth = 0;
+		int maxHeight = 0;
+
+		// 최대 가로 길이 구하기
+		for (int i = 0; i < horizon.size() - 1; i++) {
+			int curWidth = horizon.get(i + 1) - horizon.get(i);
+
+			maxWidth = Math.max(maxWidth, curWidth);
+		}
+
+		// 최대 세로 길이 구하기
+		for (int i = 0; i < vertical.size() - 1; i++) {
+			int curHeight = vertical.get(i + 1) - vertical.get(i);
+
+			maxHeight = Math.max(maxHeight, curHeight);
+		}
+
+		// 최대 넓이 구하기
+		int maxArea = maxWidth * maxHeight;
+
+		// 출력
+		System.out.println(maxArea);
+	}
+}
