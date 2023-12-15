68426d50950f8fce146564b329c61b5f6cf89e19
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-12-15 10:11:22 +0900

    [Silver V] Title: 이름 궁합, Time: 332 ms, Memory: 80496 KB -BaekjoonHub

---
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Main {
+
+	// 알파벳 획수
+	private static final int[] strokeCnt = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2,
+		1};
+
+	private static int[] nameStroke;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 종민이와 그녀의 영어 이름
+		String jongmin = sc.next();
+		String her = sc.next();
+
+		// 두 이름의 길이는 같음을 보장하므로..
+		int length = 2 * jongmin.length();
+
+		// 이름 궁합을 볼 배열
+		nameStroke = new int[length];
+
+		for (int i = 0; i < length; i++) {
+			if (i % 2 == 0) {
+				// 짝수 인덱스일 때는 종민이 알파벳 가져다 쓰기
+				int idx = i / 2;
+				int strokeIdx = jongmin.charAt(idx) - 'A';
+
+				nameStroke[i] = strokeCnt[strokeIdx];
+			} else {
+				// 홀수 인덱스일 때는 그녀의 알파벳 가져다 쓰기
+				int idx = i / 2;
+				int strokeIdx = her.charAt(idx) - 'A';
+
+				nameStroke[i] = strokeCnt[strokeIdx];
+			}
+		}
+
+		// 궁합 구하는 횟수
+		int re = length - 2;
+
+		// 반복해서 궁합 구해주기
+		while (re-- > 0) {
+			nameStroke = sum(nameStroke, nameStroke.length);
+		}
+
+		// 문자열로 받기
+		String ans = String.valueOf(nameStroke[0]) + nameStroke[1];
+
+		// 출력
+		System.out.println(ans);
+	}
+
+	// 궁합 구하는 메서드
+	private static int[] sum(int[] nameStroke, int length) {
+		int[] result = new int[length];
+
+		for (int i = 0; i < length - 1; i++) {
+			// 인접한 두 수 더하고 10으로 나눈 나머지
+			int cur = (nameStroke[i] + nameStroke[i + 1]) % 10;
+
+			// 결과 배열에 저장
+			result[i] = cur;
+		}
+
+		// 리턴
+		return result;
+	}
+}
