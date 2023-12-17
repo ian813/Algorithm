b212f10d7ee8f99c256b4a05bf1c568ed9d1bbce
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-12-17 10:00:31 +0900

    [Silver IV] Title: 30, Time: 180 ms, Memory: 16596 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,13 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		// 숫자를 문자열 형태로 입력받기
-		String num = sc.next();
+		String num = br.readLine();
 
 		// 30의 배수를 만들 수 있는지 판단할 불리안
 		// 30의 배수 -> 10의 배수 + 3의 배수 -> 0이 포함되어 있고, 각 자릿수 합이 3의 배수이면 된다
