5b645bd8880ade039a4b095c8b2c8e34b8e43fce
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-12-25 20:04:26 +0900

    [Silver V] Title: 줄 세우기, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/11536. 줄 세우기/줄 세우기.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 사람 수
+		int people = sc.nextInt();
+
+		// 선수들의 입력정보를 저장할 배열과 그걸 오름차순으로 정렬하기 위한 카피배열
+		String[] name = new String[people];
+		String[] sortName = new String[people];
+
+		// 정보 입력받기
+		for (int i = 0; i < people; i++) {
+			name[i] = sc.next();
+			sortName[i] = name[i];
+		}
+
+		// 오름차순 정렬
+		Arrays.sort(sortName);
+
+		if (isIncreasing(name, sortName)) {
+			// 오름차순과 일치하면 INCREASING 출력
+			System.out.println("INCREASING");
+		} else if (isDecreasing(name, sortName)) {
+			// 내림차순과 일치하면 DECREASING 출력
+			System.out.println("DECREASING");
+		} else {
+			// 둘 다 아니면 NEITHER 출력
+			System.out.println("NEITHER");
+		}
+	}
+
+	// 오름차순인지 판단할 메서드
+	private static boolean isIncreasing(String[] arr, String[] copyArr) {
+		for (int i = 0; i < arr.length; i++) {
+			// 비교해서 다른게 나오면 false 리턴
+			if (!arr[i].equals(copyArr[i])) {
+				return false;
+			}
+		}
+		// 그게 아니면 true 리턴
+		return true;
+	}
+
+	// 내림차순인지 판단할 메서드
+	// 판단 인덱스가 거꾸로인 거 빼고 위의 메서드와 같음
+	private static boolean isDecreasing(String[] arr, String[] copyArr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (!arr[i].equals(copyArr[arr.length - 1 - i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
