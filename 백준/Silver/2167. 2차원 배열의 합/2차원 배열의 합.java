1c4e21640b55b89d4f6a4c6c790f2021bb1d7463
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2023-12-27 17:02:00 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 2652 ms, Memory: 49124 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,36 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		// 행렬 크기
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
 		// 행렬 생성 (인덱스 맞추기 위해 N+1, M+1)
 		int[][] matrix = new int[N + 1][M + 1];
 
 		// 행렬 정보 입력받기
 		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= M; j++) {
-				matrix[i][j] = sc.nextInt();
+				matrix[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
+		st = new StringTokenizer(br.readLine());
 		// 부분 합 구할 개수
-		int K = sc.nextInt();
+		int K = Integer.parseInt(st.nextToken());
 
 		StringBuilder sb = new StringBuilder();
 
 		while (K-- > 0) {
 			// K번만큼 반복
+			st = new StringTokenizer(br.readLine());
 			// 시작 위치와 끝 위치 입력받기
-			int i = sc.nextInt();
-			int j = sc.nextInt();
-			int x = sc.nextInt();
-			int y = sc.nextInt();
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 
 			// 부분합 구할 변수
 			int sum = 0;
