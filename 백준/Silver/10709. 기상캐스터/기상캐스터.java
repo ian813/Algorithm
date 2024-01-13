5fa0f08348c5c0dccb89b9fe2f61b9406443ac91
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2024-01-13 09:48:26 +0900

    [Silver V] Title: 기상캐스터, Time: 232 ms, Memory: 18736 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 행 열 길이 입력받기
+		int row = sc.nextInt();
+		int col = sc.nextInt();
+
+		// 시간 정보 저장할 배열
+		int[][] time = new int[row][col];
+
+		// 맵에 기상정보 입력하기
+		char[][] map = new char[row][col];
+
+		for (int i = 0; i < row; i++) {
+			String str = sc.next();
+
+			// 죄측에 구름이 뜬 적 있는지 판단할 불리안
+			// 매 행마다 false로 초기화
+			boolean flag = false;
+
+			for (int j = 0; j < col; j++) {
+				map[i][j] = str.charAt(j);
+
+				if (map[i][j] == 'c') {
+					// 구름 생성되면 true로 변경
+					flag = true;
+				}
+
+				if (flag && map[i][j] == '.') {
+					// 구름이 떴고 원래 구름 있던 곳이 아니면
+					// 그 전 열의 시간에 +1
+					time[i][j] = time[i][j - 1] + 1;
+				}
+
+				if (!flag) {
+					// 구름이 좌측에 안 떴으면 시간 -1로 바꾸기
+					time[i][j] = -1;
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		// 시간을 스트링빌더에 형식메 맞게 저장하고 출력
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				sb.append(time[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
