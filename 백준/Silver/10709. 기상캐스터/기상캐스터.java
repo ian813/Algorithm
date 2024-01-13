028a9f6c2f66a8eb9d5ed19f7a76c8235b73b2fe
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2024-01-13 09:52:02 +0900

    [Silver V] Title: 기상캐스터, Time: 136 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
 		// 행 열 길이 입력받기
-		int row = sc.nextInt();
-		int col = sc.nextInt();
+		int row = Integer.parseInt(st.nextToken());
+		int col = Integer.parseInt(st.nextToken());
 
 		// 시간 정보 저장할 배열
 		int[][] time = new int[row][col];
@@ -16,7 +20,7 @@ public class Main {
 		char[][] map = new char[row][col];
 
 		for (int i = 0; i < row; i++) {
-			String str = sc.next();
+			String str = br.readLine();
 
 			// 죄측에 구름이 뜬 적 있는지 판단할 불리안
 			// 매 행마다 false로 초기화
