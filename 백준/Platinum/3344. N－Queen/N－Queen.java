68bf51f1c5d51bfce26f84fb573d4b97344a2747
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2024-03-24 17:44:18 +0900

    [Platinum II] Title: N-Queen, Time: 212 ms, Memory: 17892 KB -BaekjoonHub

--- 백준/Platinum/3344. N－Queen/N－Queen.java
+++ 백준/Platinum/3344. N－Queen/N－Queen.java
@@ -1,59 +1,50 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       StringBuilder sb = new StringBuilder();
-       
-       int N = sc.nextInt();
-       
-       if (N % 6 != 2 && N % 6 != 3) {
-          for (int i = 1; i <= N; i++) {
-             if (i % 2 != 0) {
-                sb.append(i).append("\n");
-             }
-          }
-          
-          for (int i = 2; i <= N; i++) {
-             if (i % 2 == 0) {
-                sb.append(i).append("\n");
-             }
-          }
-          
-       } else if (N % 6 == 2) {
-          for (int i = 2; i <= N; i++) {
-             if (i % 2 == 0) {
-                sb.append(i).append("\n");
-             }
-          }
-          
-          sb.append(3).append("\n");
-          sb.append(1).append("\n");
-          for (int i = 7; i <= N; i++) {
-             if (i % 2 != 0) {
-                sb.append(i).append("\n");
-             }
-          }
-          sb.append(5).append("\n");
-          
-       } else if (N % 6 == 3) {
-          for (int i = 4; i <= N; i++) {
-             if (i % 2 == 0) {
-                sb.append(i).append("\n");
-             }
-          }
-          sb.append(2).append("\n");
-          
-          for (int i = 5; i <= N; i++) {
-             if (i % 2 != 0) {
-                sb.append(i).append("\n");
-             }
-          }
-          sb.append(1).append("\n");
-          sb.append(3).append("\n");
-          
-       }
-       
-       System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		if (N % 6 != 2 && N % 6 != 3) {
+			for (int i = 1; i <= N; i += 2) {
+				sb.append(i).append("\n");
+			}
+
+			for (int i = 2; i <= N; i += 2) {
+				sb.append(i).append("\n");
+			}
+
+		} else if (N % 6 == 2) {
+			for (int i = 2; i <= N; i += 2) {
+				sb.append(i).append("\n");
+			}
+
+			sb.append(3).append("\n");
+			sb.append(1).append("\n");
+			for (int i = 7; i <= N; i += 2) {
+				sb.append(i).append("\n");
+			}
+			sb.append(5).append("\n");
+
+		} else {
+			for (int i = 4; i <= N; i += 2) {
+				sb.append(i).append("\n");
+			}
+			sb.append(2).append("\n");
+
+			for (int i = 5; i <= N; i += 2) {
+				sb.append(i).append("\n");
+			}
+			sb.append(1).append("\n");
+			sb.append(3).append("\n");
+
+		}
+
+		System.out.println(sb);
+	}
 }
