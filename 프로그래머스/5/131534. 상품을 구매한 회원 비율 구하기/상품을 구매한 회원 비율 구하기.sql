441166881c7a5e961945540af0e26004d55a7249
Euiseok Jung <118112177+ian813@users.noreply.github.com>
2024-05-02 14:56:47 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+SELECT EXTRACT(YEAR FROM SALES_DATE) YEAR
+     , EXTRACT(MONTH FROM SALES_DATE) MONTH
+     , COUNT(DISTINCT USER_ID) PUCHASED_USERS
+     , ROUND(COUNT(DISTINCT USER_ID) / (SELECT COUNT(USER_ID)
+                                            FROM USER_INFO
+                                           WHERE EXTRACT(YEAR FROM JOINED) = '2021'), 1) PUCHASED_RATIO
+  FROM ONLINE_SALE
+ WHERE USER_ID IN (
+                   SELECT USER_ID
+                     FROM USER_INFO
+                    WHERE EXTRACT(YEAR FROM JOINED) = '2021'
+                   )
+GROUP BY EXTRACT(YEAR FROM SALES_DATE)
+       , EXTRACT(MONTH FROM SALES_DATE)
+ORDER BY YEAR
+       , MONTH
+;
